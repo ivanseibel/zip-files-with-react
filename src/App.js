33e6821513f9780b66aa845fa5faf5528e2bd994
Ivan L. Seibel <me@ivanseibel.dev>
2021-09-01 20:13:33 -0300

    Implementing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { useCallback, useState } from "react";
+
+import axios from 'axios';
+import { saveAs } from 'file-saver';
+import JSZip from "jszip";
+import { span } from "prelude-ls";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleGetFile = useCallback(async () => {
+    try {
+      setIsLoading(true);
+      const tagResponse = await axios.get('https://www.correios.com.br/enviar/encomendas/arquivo/nacional/guia-tecnico-embalagens-rpc_v1-1.pdf', {
+        responseType: 'blob'
+      });
+
+      const plpResponse = await axios.get('http://santoandre.educaon.com.br/wp-content/uploads/2020/10/Manual_do_Usuario_SIGEP.pdf', {
+        responseType: 'blob'
+      });
+
+      setIsLoading(false);
+
+      const zip = new JSZip();
+      zip.file('etiqueta.pdf', tagResponse.data, { binary: true });
+      zip.file('plp.pdf', plpResponse.data, { binary: true });
+
+      zip.generateAsync({ type: 'blob' }).then(blob => {
+        saveAs(blob, 'file.zip');
+      });
+
+    } catch (error) {
+      console.log(error.message);
+    } finally {
+      setIsLoading(false);
+    }
+
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div styles="display: flex">
+      <button onClick={handleGetFile}>
+        Get file
+      </button>
+
+      <p>
+        {isLoading && (
+          <span>We are getting your files...</span>
+        )}
+      </p>
     </div>
   );
 }
